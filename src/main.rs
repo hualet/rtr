c8ccc647b9d0be49b6bd080c6bc1708e8a8ae0e1
Hualet Wang <mr.asianwang@gmail.com>
2019-04-25 21:25:22 +0800

    get icmp ping done

--- src/main.rs
+++ src/main.rs
@@ -3,43 +3,40 @@ extern crate pnet;
 use std::net::{IpAddr, Ipv4Addr};
 
 use pnet::util::checksum;
-use pnet::transport::{transport_channel, icmp_packet_iter};
-use pnet::transport::TransportChannelType::Layer3;
-
 use pnet::packet::Packet;
+use pnet::transport::{transport_channel, icmp_packet_iter};
+use pnet::transport::TransportChannelType::Layer4;
+use pnet::transport::TransportProtocol::Ipv4;
+
 use pnet::packet::ip::IpNextHeaderProtocols;
-use pnet::packet::icmp::{IcmpPacket, IcmpTypes};
+use pnet::packet::icmp::IcmpTypes;
 use pnet::packet::icmp::echo_request::{MutableEchoRequestPacket, IcmpCodes};
 
 fn main() {
-    const channel_buf_size: usize = 4096;
-    let protocol = Layer3(IpNextHeaderProtocols::Icmp);
+    const CHANNEL_BUF_SIZE: usize = 4096;
+    let protocol = Layer4(Ipv4(IpNextHeaderProtocols::Icmp));
 
-    let (mut tx, mut rx) = match transport_channel(channel_buf_size, protocol) {
+    let (mut tx, mut rx) = match transport_channel(CHANNEL_BUF_SIZE, protocol) {
         Ok((tx, rx)) => (tx, rx),
         Err(e) => panic!("error happened {}", e),
     };
 
     // tx.set_ttl(1);
 
-    let mut packet_buf = [0u8; 128];
-    let mut payload_buf = [0u8; 10];
+    // create ping packet
+    let mut packet_contents = vec![0u8; 48];
+    let mut ping_packet = MutableEchoRequestPacket::owned(packet_contents).unwrap();
+    ping_packet.set_icmp_type(IcmpTypes::EchoRequest);
+    ping_packet.set_icmp_code(IcmpCodes::NoCode);
+    ping_packet.set_identifier(0);
+    ping_packet.set_sequence_number(0);
+    ping_packet.set_payload(b"Hello rust");
 
-    let mut packet = MutableEchoRequestPacket::new(&mut packet_buf).expect("failed to new MutableEchoRequestPacket");
-    packet.set_icmp_type(IcmpTypes::EchoRequest);
-    packet.set_icmp_code(IcmpCodes::NoCode);
-    packet.set_identifier(0);
-    packet.set_sequence_number(0);
-    packet.set_checksum(0);
-    packet.set_payload(&mut payload_buf);
+    let cksm = checksum(ping_packet.packet(), 1);
+    ping_packet.set_checksum(cksm);
 
-    let cksm = checksum(packet.packet(), 1);
-    packet.set_checksum(cksm);
-
-    println!("{}", cksm);
-
-    let dest_addr = IpAddr::V4(Ipv4Addr::new(192, 168, 1, 1));
-    let sent_size = tx.send_to(packet, dest_addr).expect("failed to send the packet");
+    let dest_addr = IpAddr::V4(Ipv4Addr::new(10, 0, 0, 1));
+    let sent_size = tx.send_to(ping_packet, dest_addr).expect("failed to send the packet");
 
     println!("{} bytes has been sent", sent_size);
 
